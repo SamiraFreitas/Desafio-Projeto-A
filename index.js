dce68e6a1f21e426bc685ef01ba15c5534a2d268
Leonardo Moreira <leomoreira.com@gmail.com>
2021-04-17 00:07:46 -0300

    test post

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
+app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/',(req,res)=>res.render('pages/index'))//renderiza a página home
-  .get('/inscreva-se',(req,res)=>res.render('pages/inscreva-se'))//renderiza página escreva
-  .get('/login',(req,res)=>res.render('pages/login'))//renderiza página login
-  .get('/republicas', async (req,res)=>{//conecta o banco de dados a página de republicas
+app.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs');
+
+  app.use(express.urlencoded({extended : false}));
+
+  app.get('/',(req,res)=>res.render('pages/index'));//renderiza a página home
+  app.get('/inscreva-se',(req,res)=>res.render('pages/inscreva-se'));//renderiza página escreva
+  app.get('/login',(req,res)=>res.render('pages/login'));//renderiza página login
+  app.get('/republicas', async (req,res)=>{//conecta o banco de dados a página de republicas
     try{
     const client = await pool.connect();//conecta o banco de dados
     const result = await client.query('SELECT * FROM republicas');//seleciona tudo da tabela de teste
@@ -26,8 +29,16 @@ express()
       res.send("Erro: "+err);
     }
   })
-  
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.post('/users/inscreva-se',(req,res)=>{
+    let {name,email,password,password2}=req.body;
+    console.log(
+      name,
+      email,
+      password,
+      password2
+    )
+  })
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
  
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
