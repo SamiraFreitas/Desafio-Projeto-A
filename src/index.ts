30397676306286ccc4493346dd8319b95fac17f8
Leonardo Moreira <leomoreira.com@gmail.com>
2021-04-26 16:20:59 -0300

    Views para dentro do src

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(express.static(path.join(__dirname, "../public")));
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
